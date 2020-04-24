78a17d0e6adafea258d96cbd0cefa268dc979fe0
docfate111 <tdwilliamsiv@gmail.com>
2020-04-24 15:30:28 -0700

    first commit work in progress

---
+++ build/precache-manifest.d4d2e6a2b52d62255bcc7ca6b3e3c500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4545105c32fa47d1a86a94b58fbbb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4f3df495dbb06591f6",
+    "url": "/static/css/main.29800258.chunk.css"
+  },
+  {
+    "revision": "fbeccc32d04b217d60e4",
+    "url": "/static/js/2.b783b21f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b783b21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4f3df495dbb06591f6",
+    "url": "/static/js/main.f453a937.chunk.js"
+  },
+  {
+    "revision": "6c9b6cc8a6d24a7a3125",
+    "url": "/static/js/runtime-main.25700bef.js"
+  }
+]);
